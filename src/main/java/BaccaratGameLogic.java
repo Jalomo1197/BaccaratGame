73b6e90d12be0edf8cf1cc962b7a9482ff9150e7
hanna <hannathayyil96@gmail.com>
2019-10-06 18:36:49 -0500

    adding class files

---
+++ src/main/java/BaccaratGameLogic.java
@@ -0,0 +1,19 @@
+
+
+public class BaccaratGameLogic {
+	public String whoWon(ArrayList<Card> hand1, ArrayList<Card> hand2) {
+		//The method whoWon will evaluate two hands at the end of the game 
+		//and return a string depending on the winner: “Player”, “Banker”, “Draw”.
+	}
+	public int handTotal(ArrayList<Card> hand) {
+		//take a hand and return how many points that hand is worth.
+	}
+	public boolean evaluateBankerDraw(ArrayList<Card> hand, Card playerCard) {
+		//return true if Banker should be dealt a third card, otherwise return false.
+	}
+	public boolean evaluatePlayerDraw(ArrayList<Card> hand) {
+		//return true if Player should be dealt a third card, otherwise return false.
+	}
+	
+	
+}

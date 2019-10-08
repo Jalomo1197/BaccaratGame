545fdd6f71758b336c0362cea8f9c59ef749e54d
hanna <hannathayyil96@gmail.com>
2019-10-08 17:49:05 -0500

    adding class files

--- src/main/java/BaccaratDealer.java
+++ src/main/java/BaccaratDealer.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class BaccaratDealer {
 	ArrayList<Card> deck;
@@ -5,6 +7,13 @@ public class BaccaratDealer {
 	public void generateDeck() {
 		//generate a new standard 52 card deck
 		//where each card is an instance of the Card class in the ArrayList<Card> deck.
+		deck = new ArrayList<Card>();
+		for (int suit = 0; suit <= 3; suit++) {
+			for (int val = 0; val <= 13; val++ ) {
+				Card c = new Card(suit, val); 
+				deck.add(c);
+			}
+		}
 	}
 	public ArrayList<Card> dealHand(){
 		//ill deal two cards and return them in an ArrayList<Card>
@@ -16,9 +25,11 @@ public class BaccaratDealer {
 	public void shuffleDeck() {
 		//will create a new deck of 52 cards and “shuffle”
 		//randomize the cards in that ArrayList<Card>
+		Collections.shuffle(deck);
 	}
 	public int deckSize() {
 		//will just return how many cards are in this.deck at any given time.
+		deck.size();
 	}
 	
 }
--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,11 +1,68 @@
 
 public class Card {
-	String suite;
-	int value;
+	private String suit; //Spades, hearts, diamonds, clubs
+	
+						//ENUMERATED SUITS: 
+						//SPADES = 0
+						//HEARTS = 1
+						//DIAMONDS =2
+						//CLUBS = 3
+						
+	
+	private int value; //from 1-13
 
 	//CONSTRUCTOR
-	Card(String theSuite, int theValue){
-		this.suite = theSuite;
+	Card(String theSuit, int theValue){
+		this.suit = theSuit;
 		this.value = theValue;
 	}
+	
+	//card overload constructor with suit as int 
+	//for easy initialization 
+	Card(int suit, int theValue){
+		if (suit == 0) {
+			this.suit = "Spades";
+		}
+		else if (suit == 1) {
+			this.suit = "Hearts";
+		}
+		else if (suit == 2) {
+			this.suit = "Diamonds";
+		}
+		else if (suit == 3) {
+			this.suit = "Clubs";
+		}
+		this.value = theValue;
+		
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	
+	public String getSuit() {
+		return suit;
+	}
+	
+	public String getValueAsString() {
+		switch(value) {
+		case 1:  return "Ace";
+		case 2:  return "2";
+		case 3:  return "3";
+		case 4:  return "4";
+		case 5:  return "5";
+		case 6:  return "6";
+		case 7:  return "7";
+		case 8:  return "8";
+		case 9:  return "9";
+		case 10: return "10";
+		case 11: return "Jack";
+		case 12: return "Queen";
+		case 13: return "King";
+		default: return "Not a card";
+		
+		}
+	}
+	
 }

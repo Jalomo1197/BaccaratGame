665d910ce7f131bf7c295781484ab4cdd64e472c
Jalomo1197 <alexisjalomo1197@gmail.com>
2019-10-10 14:32:09 -0500

    update

--- src/main/java/BaccaratDealer.java
+++ src/main/java/BaccaratDealer.java
@@ -3,38 +3,41 @@ import java.util.Collections;
 
 public class BaccaratDealer {
 	ArrayList<Card> deck;
-	
+
 	public void generateDeck() {
 		//generate a new standard 52 card deck
 		//where each card is an instance of the Card class in the ArrayList<Card> deck.
 		deck = new ArrayList<Card>();
 		for (int suit = 0; suit <= 3; suit++) {
 			for (int val = 0; val <= 13; val++ ) {
-				Card c = new Card(suit, val); 
+				Card c = new Card(suit, val);
 				deck.add(c);
 			}
 		}
 	}
+
+	public bool EnoughtCard(){
+		return (this.deckSize() < 6)? false : true;
+	}
+
 	public ArrayList<Card> dealHand(){
 		//will deal two cards and return them in an ArrayList<Card>
-		
-		Card c1 = deck.get(0);
-		Card c2 = deck.get(1);
+		Card c1 = deck.remove(0);
+		Card c2 = deck.remove(0);  //MAY NOT WORK,
 		ArrayList<Card> ret = new ArrayList<Card>();
 		ret.add(c1);
 		ret.add(c2);
 		deck.remove(0);
 		deck.remove(1);
 		return ret;
-		
 	}
-	
+
 	public Card drawOne() {
 		//drawOne will deal a single card and return it.
 		Card c = deck.get(0);
 		deck.remove(0);
 		return c;
-		
+
 	}
 	public void shuffleDeck() {
 		//will create a new deck of 52 cards and “shuffle”
@@ -45,5 +48,5 @@ public class BaccaratDealer {
 		//will just return how many cards are in this.deck at any given time.
 		return deck.size();
 	}
-	
+
 }
